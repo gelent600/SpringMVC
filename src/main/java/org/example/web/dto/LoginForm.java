4f377b5abebf113161a0bf830489c939780f1f2e
Alex <gelent600@gmail.com>
2022-07-26 17:37:43 +0300

    Initial commit. I created MVC project Book shelf

---
+++ src/main/java/org/example/web/dto/LoginForm.java
@@ -0,0 +1,37 @@
+package org.example.web.dto;
+
+public class LoginForm {
+    private String username;
+    private String password;
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public LoginForm(){}
+
+    public String getName() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.username = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "name='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
